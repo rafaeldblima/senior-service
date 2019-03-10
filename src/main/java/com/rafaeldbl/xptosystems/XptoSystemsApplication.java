b1f1b405d7f67f95f8421176b3be78df475de0fc
Rafael Lima <rafaeldblima@gmail.com>
2019-03-10 13:55:49 -0400

    Estrutura básica do projeto e dependências

---
+++ src/main/java/com/rafaeldbl/xptosystems/XptoSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldbl.xptosystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptoSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XptoSystemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaeldbl/xptosystems/XptoSystemsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rafaeldbl.xptosystems;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XptoSystemsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
